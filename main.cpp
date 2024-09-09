d3a7110a2967546f34514e9e8f8079a7d612ec42
Kirill Belyaev <kirill10121415@gmail.com>
2024-09-09 11:23:34 +0300

    main.cpp completed

--- main.cpp
+++ main.cpp
@@ -1,11 +1,10 @@
-#include "iostream"
 #include "application.h"
+#include "array.h"
 
 using namespace std;
 
 int main(){
-
-
-
+    Application app = Application();
+    app.exec();
     return 0;
 }
