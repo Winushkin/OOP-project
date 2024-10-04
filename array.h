0f693d91e7ab5c7fd77c47769477b20769dc1b0a
lazarevFedor <lazarevfedor@gmail.com>
2024-10-04 11:09:11 +0300

    Добавил энтеры между прототипами методов, чтобы был единый код-стайл

--- array.h
+++ array.h
@@ -14,15 +14,26 @@ private:
 
 public:
     Array(int length = 0);
+
     ~Array();
+
     int getLength();
+
     void fill();
+
     bool resize(int length);
+
     bool changeElement(int index, number value);
+
     void printArray();
+
     number averageValue();
+
     number SKO();
+
     void shakerSort(bool reverse = false);
+
+
 };
 
-#endif //OOPPRACTICE_ARRAY_H
+#endif
